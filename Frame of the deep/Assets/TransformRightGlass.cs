6d9776f8e32a9fe2656ba662f988010b06280d1f
litvinas <melynasdangus@gmail.com>
2016-11-23 12:46:05 +0100

    attached a script on an object for prototyping. not the actual building, just for testing.
    attached a script on an object for prototyping. not the actual building,
    just for testing.

---
+++ Frame of the deep/Assets/TransformRightGlass.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformRightGlass : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public Vector3 targetPosition;
+    public Quaternion targetRotation; //Optional of course
+    public float smoothFactor = 2;
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothFactor);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * smoothFactor);
+
+    }
+}
+
+//         transform.Translate(20 * Time.deltaTime/5, 0, 0);
