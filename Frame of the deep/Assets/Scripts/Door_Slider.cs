bb29ca7d4474ec2b6069091cfe0a318a51e2d260
Anders Elmholdt <anders@elmholdt.dk>
2016-11-23 14:38:28 +0100

    Added a random door slider script

---
+++ Frame of the deep/Assets/Scripts/Door_Slider.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door_Slider : MonoBehaviour {
+
+    public bool isOpen;
+    int place = 0;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Random.value > 0.995f)
+        {
+            isOpen = !isOpen;
+        }
+	    if (isOpen)
+        {
+            if (place < 40)
+            {
+                transform.Translate(-0.02f, 0, 0);
+                place++;
+            }
+        } else
+        {
+            if (place >= 0)
+            {
+                transform.Translate(0.02f, 0, 0);
+                place--;
+            }
+        }
+	}
+}
