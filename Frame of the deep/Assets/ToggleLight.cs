7294499c56d2ae9c30753b8e6fb73a59b47102ad
ThatGuyWithTheBeard <nicklas.kristoffersen@hotmail.com>
2016-11-23 15:29:43 +0100

    Light Script Added
    Intervals of lights switching on/off to be added shortly.

---
+++ Frame of the deep/Assets/ToggleLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleLight : MonoBehaviour {
+	
+	public bool isOn;
+	public Light light;
+
+	// Use this for initialization
+	void Start () {
+		light.GetComponent<Light>();
+		light.intensity = 0.0f;
+		isOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(isOn){
+			light.intensity = 6.0f;
+		} else if(!isOn) {
+			light.intensity = 0.0f;
+		}
+
+	
+	}
+
+}
