b23c900369363ba65246f6fc0896261a551ac1bf
ThatGuyWithTheBeard <nicklas.kristoffersen@hotmail.com>
2016-11-24 02:33:50 +0100

    Lights re-added
    Doesn't switch on/off randomly at current time.

--- Frame of the deep/Assets/ToggleLight.cs
+++ Frame of the deep/Assets/ToggleLight.cs
@@ -4,25 +4,36 @@ using System.Collections;
 public class ToggleLight : MonoBehaviour {
 	
 	public bool isOn;
-	public Light light;
+	//public Light light;
 
 	// Use this for initialization
 	void Start () {
-		light.GetComponent<Light>();
-		light.intensity = 0.0f;
+		//light.GetComponent<Light>();
+		//light.intensity = 0.0f;
 		isOn = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if(isOn){
-			light.intensity = 6.0f;
-		} else if(!isOn) {
-			light.intensity = 0.0f;
+	//Randomly switches between on/off states
+		int count = 0;
+
+		if (Random.value > 0.995f){
+			count++;
 		}
 
-	
-	}
+		if(count == 5){
+			isOn = !isOn;
+			count = 0;
+		}
 
+		if(isOn){
+            GetComponent<Light>().intensity = 6.0f;
+		} else if(!isOn) {
+            GetComponent<Light>().intensity = 0.0f;
+		}
+
+		
+	}
 }
