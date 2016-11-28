3ae4afbb84ceea0fb837990d8cf77f3d62442a8f
Anders Elmholdt <anders@elmholdt.dk>
2016-11-28 12:57:57 +0100

    Changed the sunration

--- Frame of the deep/Assets/Midterm/SunRotation.cs
+++ Frame of the deep/Assets/Midterm/SunRotation.cs
@@ -10,6 +10,13 @@ public class SunRotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Vector3.right * Time.deltaTime * 10, Space.World);
+        if (transform.rotation.eulerAngles.x % 360 > 180)
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * 40, Space.World);
+        }
+        else
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * 10, Space.World);
+        }
     }
 }
